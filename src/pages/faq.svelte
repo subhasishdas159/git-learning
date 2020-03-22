f37468f0903a52f27536dec93a766b2d304a3b2c
Subhasish Das <subhasishdas159@gmail.com>
2020-03-22 22:19:48 +0530

    okay again

--- src/pages/faq.svelte
+++ src/pages/faq.svelte
@@ -6,8 +6,5 @@
 <Lead />
 <Lead2 />
 
-<<<<<<< HEAD
 I hope this is not much of a problem. now creatinf issue!
-=======
 This is bound to cause trouble okay it already was resolved
->>>>>>> very-bad
