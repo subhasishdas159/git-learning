c13b2c3b04fa84f025b80c46a0391d26d7028d72
Subhasish Das <subhasishdas159@gmail.com>
2020-03-22 22:46:40 +0530

    hello

--- src/pages/faq.svelte
+++ src/pages/faq.svelte
@@ -10,10 +10,5 @@ I hope this is not much of a problem. now creatinf issue!
 This is bound to cause trouble okay it already was resolved
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
-Modifying it now c
-=======
-Modifying it now b
->>>>>>> Stashed changes
-=======
-Modifying it now b
->>>>>>> Stashed changes
+<<<<<<< Updated upstream
+Modifying it now
